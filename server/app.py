19c0ab3cddabfd85517bcd7b3bbb87037fc2c614
Daniel Long <danielwlong618@gmail.com>
2024-06-11 14:21:12 -0500

    lab done

--- server/app.py
+++ server/app.py
@@ -20,7 +20,39 @@ def index():
     return "Index for Game/Review/User API"
 
 # start building your API here
+@app.route('/games')
+def games():
+    games = [game.to_dict() for game in Game.query.all()]
+    # for game in Game.query.order_by(Game.title).all():
+    #     game_dict = game.to_dict()
+    #     games.append(game_dict)
 
+    response = make_response(games, 200)
+
+    return response
+
+@app.route('/games/<int:id>')
+def games_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(game_dict, 200)
+    return response 
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    users = [review.user.to_dict(rules=('-reviews',)) for review in game.users]
+
+    # for review in game.reviews:
+    #     user = review.user
+    #     user_dict = user.to_dict(rules=("-reviews",))
+    #     users.append(user_dict)
+
+    response = make_response(users, 200)
+    return response 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
